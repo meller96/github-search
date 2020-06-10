dbedb30d8c89061889fde70f0876207e694df6ce
meller96 <meller70@gmail.com>
2020-06-10 22:42:46 +0300

    add sorting field and fix repoComponent width

--- src/app/components/search/sort-by-field.pipe.ts
+++ src/app/components/search/sort-by-field.pipe.ts
@@ -13,6 +13,8 @@ export class SortByFieldPipe implements PipeTransform {
       case 'date':
         value.sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
         return value
+      case 'forks':
+        value.sort((a, b) => (a.name > b.name) ? 1 : -1)
       default: return value
     }
   }
